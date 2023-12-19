7c229df5e3efda6424e34e7908699e1e4042c184
A.Bekkine <abekkine@gmail.com>
2023-12-19 22:46:09 +0300

    Dimensions arranged ... :
    - ... to work /w smaller numbers.

--- main.cpp
+++ main.cpp
@@ -9,14 +9,25 @@ const double k_EarthOrbitalSpeed = 2.9783e4;
 const double k_Pi = 3.14159265358979323846;
 const double k_Rad2Deg = 180.0 / k_Pi;
 const double k_Deg2Rad = k_Pi / 180.0;
-const double dt = 1;
+
+const double cDistanceFactor = 0.0001;
+const double cMassFactor = k_GravitationalConstant * 1e-6;
+const double cSpeedFactor = 0.01;
+const double cT = cDistanceFactor / cSpeedFactor;
+
+const double dt = cT;
+const double c_SunMass = k_SunMass * cMassFactor * cSpeedFactor * cSpeedFactor * cDistanceFactor;
+const double c_EarthMass = k_EarthMass * cMassFactor * cSpeedFactor * cSpeedFactor * cDistanceFactor;
+const double c_GravitationalConstant = k_GravitationalConstant / cMassFactor;
+const double c_AstronomicalUnit = k_AstronomicalUnit * cDistanceFactor;
+const double c_EarthOrbitalSpeed = k_EarthOrbitalSpeed * cSpeedFactor;
 
 void simulation_A() {
     // Initial.
-    double x = k_AstronomicalUnit;
+    double x = c_AstronomicalUnit;
     double y = 0.0;
     double vx = 0.0;
-    double vy = k_EarthOrbitalSpeed;
+    double vy = c_EarthOrbitalSpeed;
 
     // Loop
     int quit_count = 0;
@@ -26,7 +37,7 @@ void simulation_A() {
     double r_min = 1e30;
     double r_max = -1e30;
     while (quit_count < 5) {
-        a = k_SunMass * k_GravitationalConstant / (x * x + y * y);
+        a = c_SunMass * c_GravitationalConstant / (x * x + y * y);
         r = sqrt(x * x + y * y);
 
         if (r < r_min) {
@@ -54,16 +65,25 @@ void simulation_A() {
         if (d_alpha > 10.0) {
             alpha_old = alpha;
             printf(".");
-            // printf("%.1f, %.1f @ %.1f\n", alpha, r * 0.001, elapsed);
         } else if (d_alpha < 0.0) {
             alpha_old = alpha;
             ++quit_count;
-            printf("%.1f, %.1f @ %.1f\n", alpha, r * 0.001, elapsed / (3600 * 24));
-            printf("r_min = %.1f km\n", r_min * 0.001);
-            printf("r_max = %.1f km\n", r_max * 0.001);
-            puts("===");
+            puts("");
+            printf("r_min(%.1f), r_max(%.1f), elapsed(%.1f)\n"
+                , r_min * 0.001
+                , r_max * 0.001
+                , elapsed / (3600 * 24 * cT)
+            );
         }
+
     }
+    puts("===");
+    printf("Sun Mass : %.1f kg\n", c_SunMass);
+    printf("Earth Mass : %.1f kg\n", c_EarthMass);
+    printf("Gravitational Constant : %.1f m^3 / (kg * s^2)\n", c_GravitationalConstant);
+    printf("Astronomical Unit : %.1f km\n", c_AstronomicalUnit * 1e-3);
+    printf("Earth Orbital Speed : %.1f km / s\n", c_EarthOrbitalSpeed * 1e-3);
+    printf("Time Factor : %.4f\n", cT);
 }
 
 int main() {
